b8cd15dd3877315b2268d502cbc64a25a915eddc
marshabilqiisnasywaa <marshabilqiisnasywaa2005@mail.ugm.ac.id>
2024-08-25 15:49:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myloginpage/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myloginpage
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val usernameInput = findViewById<EditText>(R.id.username)
+        val passwordInput = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+    }
+}
